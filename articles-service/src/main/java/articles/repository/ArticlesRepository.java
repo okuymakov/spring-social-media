d13e4e18657e9120425cf4360b05274daadbbd91
okuymakov <oleg.kuymakov@bk.ru>
2022-08-16 22:43:05 +0300

    add filtering by fields

--- articles-service/src/main/java/articles/repository/ArticlesRepository.java
+++ articles-service/src/main/java/articles/repository/ArticlesRepository.java
@@ -2,8 +2,29 @@ package articles.repository;
 
 import articles.model.Article;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 
-public interface ArticlesRepository extends MongoRepository<Article, String> { }
+public interface ArticlesRepository extends MongoRepository<Article, String> {
+
+    @Query("{'tags' : {$in : ?0}, 'isPrivate' : false}")
+    List<Article> findByTags(List<String> tags);
+
+    @Query("{'title' : ?0, 'isPrivate' : false}")
+    Article findByTitle(String title);
+
+    @Query("{'theme' : ?0, 'isPrivate' : false}")
+    List<Article> findByTheme(String theme);
+
+    @Query("{'username' : ?0, 'isPrivate' : ?1}")
+    List<Article> findByUsername(String username, boolean isPrivate);
+
+    @Query("{ $or: [{'theme': ?0}, {'tags' : {$in : ?1}}], 'isPrivate' : false}")
+    List<Article> findByThemeAndTags(String theme, List<String> tags);
+
+    @Query("{'isPrivate' : false}")
+    List<Article> findAllPublic();
+
+}
