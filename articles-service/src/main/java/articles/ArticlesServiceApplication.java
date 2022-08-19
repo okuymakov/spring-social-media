988635e4b59173827b21d1c2a9b9568588c3e617
okuymakov <oleg.kuymakov@bk.ru>
2022-08-19 21:13:05 +0300

    enable auditing

--- articles-service/src/main/java/articles/ArticlesServiceApplication.java
+++ articles-service/src/main/java/articles/ArticlesServiceApplication.java
@@ -3,10 +3,12 @@ package articles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableMongoAuditing
 public class ArticlesServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticlesServiceApplication.class, args);
